794d7d0d2d1a6fecdef6b8189638e3eb0329b3b2
ryusangwon <r8uuuuuuuu@gmail.com>
2021-08-13 14:37:04 +0900

    Set Server

--- host/src/main/java/com/example/host/MainActivity.java
+++ host/src/main/java/com/example/host/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.host;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.net.ServerSocket;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        startService(new Intent(this, ServerSocket.class));
     }
 }
--- host/src/main/java/com/example/host/MyService.java
+++ host/src/main/java/com/example/host/MyService.java
@@ -1,4 +1,71 @@
 package com.example.host;
 
-public class MyService {
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MyService extends Service {
+    
+    private static final String TAG = "[SOCKET] Service";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Log.d(TAG, "onCreate: ");
+        ServerThread serverThread = new ServerThread();
+        serverThread.start();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand: ");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "onDestroy: ");
+        super.onDestroy();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.d(TAG, "onBind: ");
+        return null;
+    }
+    
+    class ServerThread extends Thread{
+        @Override
+        public void run() {
+            Log.d(TAG, "run: ");
+            int port = 5672;
+
+            try{
+                ServerSocket serverSocket = new ServerSocket(port);
+
+                while(true){
+                    Log.d(TAG, "run: ");
+                    Socket socket = serverSocket.accept();
+
+                    ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+                    oos.writeObject("Hello, World!");
+                    oos.flush();
+
+                    socket.close();
+                }
+
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
 }
