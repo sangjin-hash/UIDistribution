d00e4483833519f35440af97ed37e45fe08fcd5d
ryusangwon <r8uuuuuuuu@gmail.com>
2021-08-13 14:39:37 +0900

    Set Server Manifest

--- host/src/main/java/com/example/host/MainActivity.java
+++ host/src/main/java/com/example/host/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         startService(new Intent(this, ServerSocket.class));
     }
 }
