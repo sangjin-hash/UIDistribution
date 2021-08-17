bf306bd7da7c16367769f06d2f034f353e8257c1
sangjin-hash <lsj9873@ajou.ac.kr>
2021-08-17 20:42:15 +0900

    UI data 전송 구현 중

--- app/src/main/java/com/example/uidistribution/MainActivity.java
+++ app/src/main/java/com/example/uidistribution/MainActivity.java
@@ -6,11 +6,14 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.host.IServiceInterface;
 
@@ -19,6 +22,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private static final String TAG = "[SOCKET] Server";
 
     IServiceInterface myService;
+
+    private TextView text1;
+    private Button button1;
+
+    private final int flag_text = 1;
+    private final int flag_btn = 2;
 
     final ServiceConnection mConnection = new ServiceConnection() {
         @Override
@@ -38,6 +47,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text1 = (TextView)findViewById(R.id.text1);
+        button1 = (Button)findViewById(R.id.button1);
+
+        text1.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                //AIDL의 메소드를 호출해서 text1의 String text값과 int textSize값 Host로 보냄.
+                String text = text1.getText().toString();
+                int text_size = pxToDp((int) text1.getTextSize());
+                try {
+                    //이거 아닌거같음.
+                    myService.getFlag(flag_text);
+                    myService.getStringText(text);
+                    myService.getSizeOfText(text_size);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                    Log.d(TAG,"onLongclick에서 RemoteException 발생");
+                }
+                return true;
+            }
+        });
+
+        button1.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                return true;
+            }
+        });
     }
 
     @Override
@@ -54,9 +92,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 //getApplicationContext().bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
                 bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
                 Log.d(TAG, "onCreate: Bind Service");
-
                 break;
-            case R.id.button:
+
+            case R.id.btn_Service:
                 try {
                     Log.d(TAG, "onCreate: Start Service from Server?");
                     myService.serviceThreadStart();
@@ -67,5 +105,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
                 break;
         }
+    }
+
+    public int pxToDp(int px)
+    {
+        return (int)(px/ Resources.getSystem().getDisplayMetrics().density);
     }
 }
