4e7471f22d378d63870f86e9eeec8989fc2ade8a
sangjin-hash <lsj9873@ajou.ac.kr>
2021-08-17 20:44:36 +0900

    AIDL & Service

--- host/src/main/java/com/example/host/MainActivity.java
+++ host/src/main/java/com/example/host/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.host;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 
 import java.net.ServerSocket;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
