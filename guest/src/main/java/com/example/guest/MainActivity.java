dd17a6e5057ab42d5c1e9d43bd0078cecd59bd23
sangjin-hash <lsj9873@ajou.ac.kr>
2021-08-26 23:43:28 +0900

    Guest thread Modularization

--- guest/src/main/java/com/example/guest/MainActivity.java
+++ guest/src/main/java/com/example/guest/MainActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
+import android.renderscript.ScriptGroup;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -24,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "[SOCKET] Guest";
 
     private int port = 5672;
+    private String ip = "192.168.0.21";
     private int maxBufferSize = 1024;
 
     private Handler mHandler = new Handler();
@@ -33,60 +37,70 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        container = (LinearLayout)findViewById(R.id.layout);
+        container = (LinearLayout) findViewById(R.id.layout);
 
-        SocketConnect client = new SocketConnect();
+        ClientThread client = new ClientThread();
         client.start();
     }
 
-    class SocketConnect extends Thread{
+    class ClientThread extends Thread {
         @Override
         public void run() {
-            try{
-                ServerSocket serverSocket = new ServerSocket(port);
-
-                while(true){
-                    Socket socket = serverSocket.accept();
-                    Log.d(TAG, "run: accept!");
-
-                    byte[] recvBuffer = new byte[maxBufferSize];
-                    InputStream is = socket.getInputStream();
-                    int num = is.read(recvBuffer);
-
-                    if(num > 0){
-                        Log.d(TAG, "Trigger Event Occured in Server");
-                        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(recvBuffer);
-                        DataInputStream dataInputStream = new DataInputStream(byteArrayInputStream);
-
-                        String getString = dataInputStream.readUTF();
-                        int getSize = dataInputStream.readInt();
-
-                        mHandler.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                    textView(getString, getSize);
-                                }
-                        });
-                        socket.close();
-                        Log.d(TAG,"Socket closed");
-                    }
-                    else{
-                        Log.d(TAG, "Not Trigger Event Occured");
-                        socket.close();
-                    }
+            while (true) {
+                try {
+                    Socket socket = new Socket(ip, port);
+                    InputThread inputThread = new InputThread(socket);
+                    inputThread.start();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
-                Log.d(TAG, "run: Connection failed");
             }
         }
     }
 
-    public void textView(String txt, int txt_size){
-        TextView view1 = new TextView(this);
+    class InputThread extends Thread {
+
+        private Socket socket;
+
+        public InputThread(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                byte[] recvBuffer = new byte[maxBufferSize];
+                InputStream is = socket.getInputStream();
+                int num = is.read(recvBuffer);
+
+                if (num > 0) {
+                    Log.d(TAG, "Trigger Event Occured in Server");
+                    ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(recvBuffer);
+                    DataInputStream dataInputStream = new DataInputStream(byteArrayInputStream);
+
+                    String getString = dataInputStream.readUTF();
+                    int getSize = dataInputStream.readInt();
+
+                    mHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            EditText(getString, getSize);
+                        }
+                    });
+                } else{
+                    Log.d(TAG,"Trigger Event NOT occured");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void EditText(String txt, int txt_size) {
+        EditText view1 = new EditText(this);
         view1.setText(txt);
         view1.setTextSize(txt_size);
-        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT);
         view1.setLayoutParams(lp);
         container.addView(view1);
